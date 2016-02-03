0380dc202e0ff1c714ea6ebadb6dfe09eca029d9
Jo Øivind Gjernes <jodleif@gmail.com>
2016-02-03 14:46:27 +0100

    FEAT: Tomt vindu.

--- src/jodleif/Main.java
+++ src/jodleif/Main.java
@@ -1,8 +1,39 @@
 package jodleif;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+public class Main extends Application
+{
+	private final BorderPane hovedLayout = new BorderPane();
+	private final Canvas tegneOmråde = new Canvas();
+	private final static double WIDTH = 800;
+	private final static double HEIGHT = 600;
+
+	@Override
+	public void start(Stage primaryStage) throws Exception
+	{
+		Scene scene = new Scene(hovedLayout, WIDTH, HEIGHT);
+
+		settOppGui();
+
+		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
+		primaryStage.show();
+	}
+
+	private void settOppGui()
+	{
+		hovedLayout.setCenter(tegneOmråde);
+		tegneOmråde.setWidth(WIDTH-100);
+		tegneOmråde.setHeight(HEIGHT-100);
+	}
+
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
 }
