79613bf8f1b21bc8907b07d7bd7f303ca69d127c
Jo Øivind Gjernes <jodleif@gmail.com>
2016-02-03 14:56:43 +0100

    FEAT: Fungerende prototype

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # AlgdatOblig2
-Obligatorisk øving. 2 i alg.dat
+Obligatorisk øving 2 i Algoritmer og Datastrukturer.
 
 [Øving 2](http://www2.hit.no/af/ifim/kurs/kurs5610/modul2/leksjon21/)
--- src/jodleif/Main.java
+++ src/jodleif/Main.java
@@ -3,6 +3,7 @@ package jodleif;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -10,8 +11,10 @@ public class Main extends Application
 {
 	private final BorderPane hovedLayout = new BorderPane();
 	private final Canvas tegneOmråde = new Canvas();
+	private final GraphicsContext grafikk = tegneOmråde.getGraphicsContext2D();
 	private final static double WIDTH = 800;
 	private final static double HEIGHT = 600;
+	private final static double VEKSTFAKTOR = 0.666;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception
@@ -19,7 +22,7 @@ public class Main extends Application
 		Scene scene = new Scene(hovedLayout, WIDTH, HEIGHT);
 
 		settOppGui();
-
+		tegnGrein((WIDTH-100)/2.0,HEIGHT-100,150,0);
 		primaryStage.setScene(scene);
 		primaryStage.setResizable(false);
 		primaryStage.show();
@@ -30,6 +33,18 @@ public class Main extends Application
 		hovedLayout.setCenter(tegneOmråde);
 		tegneOmråde.setWidth(WIDTH-100);
 		tegneOmråde.setHeight(HEIGHT-100);
+	}
+
+	private void tegnGrein(double x0, double y0, double length, double vinkel)
+	{
+		if(length <=5.0){
+			return;
+		}
+		double nyX = x0-length*Math.sin(vinkel);
+		double nyY = y0-length*Math.cos(vinkel);
+		grafikk.strokeLine(x0, y0, nyX,nyY);
+		tegnGrein(nyX,nyY, length*VEKSTFAKTOR, vinkel+0.34);
+		tegnGrein(nyX,nyY, length*VEKSTFAKTOR, vinkel-0.34);
 	}
 
 	public static void main(String[] args)
