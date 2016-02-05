c33e875be58916dcb3f7472150860b9e07c0c3a2
Jo Øivind Gjernes <jodleif@gmail.com>
2016-02-05 15:51:17 +0100

    MISC: mer javadoc

--- src/jodleif/Logikk/Tegnbar.java
+++ src/jodleif/Logikk/Tegnbar.java
@@ -8,8 +8,25 @@ import java.util.ArrayList;
  */
 public interface Tegnbar
 {
+	/**
+	 * En liste med lister av "BasisObjekter"
+	 * linje:
+	 * double[] {x0,y0,x1,y1}
+	 *
+	 * triangel:
+	 * double[] {
+	 * 	x0,y0,
+	 * 	x1,y1,
+	 * 	x2,y2
+	 * 	}
+	 * @return Liste med double[] definert som i beskrivelsen over
+	 */
 	ArrayList<ArrayList<double[]>> getPunkter();
 
+	/**
+	 * antall "nivåer" i det Tegnbare objektet, der det gir mening
+	 * @return getPunkter().size() (antall lister med punkter, der en liste representerer ett nivå!)
+	 */
 	int getHøyde();
 
 	/**
