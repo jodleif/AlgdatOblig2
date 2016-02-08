eb37892fbf8a5f0806bdd3d777a33822bd160fac
Jo Øivind Gjernes <jodleif@gmail.com>
2016-02-08 17:13:11 +0100

    MISC: Ryddet opp i koden for "introbildet",  og lagt til mer Javadoc

--- src/jodleif/Render/Intro.java
+++ src/jodleif/Render/Intro.java
@@ -17,44 +17,76 @@ import java.util.TimerTask;
 public class Intro
 {
 
-	private static final String target = "Jo Øivind Gjernes og Magnus Poppe Wang";
+	private static final String målTekst = "Jo Øivind Gjernes og Magnus Poppe Wang";
 
 	public Canvas tegnBokstaver()
 	{
 		Canvas nyttCanvas = Plotter.lagNyttTegneområde();
 		nyttCanvas.getGraphicsContext2D().setFont(new Font(40));
-		ArrayList<BokstavTimer> taskListe = new ArrayList<>();
-		StringBuilder strBld = new StringBuilder();
 
-		for(int i=0;i<target.length();++i){
-			byggRandomString(strBld,i);
-			taskListe.add(new BokstavTimer(strBld.toString(), nyttCanvas.getGraphicsContext2D()));
-			if(i!=target.length()-1) strBld.setLength(0);
-		}
+		ArrayList<BokstavTimer> oppgaveListe =
+			ekspanderStreng(nyttCanvas.getGraphicsContext2D());
 
-		for(int i=0;i<target.length();++i){
-			strBld.setCharAt(i, target.charAt(i));
-			taskListe.add(new BokstavTimer(strBld.toString(), nyttCanvas.getGraphicsContext2D()));
-		}
-		utførTimerList(taskListe, 100);
+		oppgaveListe.addAll(erstattMedBokstaver(nyttCanvas.getGraphicsContext2D()));
+
+		utførTimerList(oppgaveListe, 100);
 		return nyttCanvas;
 	}
 
+	/**
+	 * Oppretter oppgaver som gradvis bygger en streng med tilfeldige bokstaver
+	 * @param kontekst grafisk kontekst oppgaven skal tegnes på
+	 * @return liste over oppgaver
+	 */
+	private ArrayList<BokstavTimer> ekspanderStreng(GraphicsContext kontekst)
+	{
+		ArrayList<BokstavTimer> oppgaveListe = new ArrayList<>() ;
+		StringBuilder strBld = new StringBuilder();
+		for(int i = 0; i< målTekst.length(); ++i){
+			byggRandomString(strBld,i);
+			oppgaveListe.add(new BokstavTimer(strBld.toString(), kontekst));
+			if(i!= målTekst.length()-1) strBld.setLength(0);
+		}
+		return oppgaveListe;
+	}
+
+	/**
+	 * Oppretter oppgaver som gradvis erstatter tilfeldige bokstaver med "ønskede" bokstaver
+ 	 * @param kontekst grafikk-kontekst for canvaset oppgaven skal utføres på
+	 * @return Liste med oppgaver
+	 */
+	private ArrayList<BokstavTimer> erstattMedBokstaver(GraphicsContext kontekst)
+	{
+		ArrayList<BokstavTimer> oppgaveListe = new ArrayList<>() ;
+		StringBuilder strBld = new StringBuilder(målTekst);
+		for(int i = 0; i< målTekst.length(); ++i){
+			strBld.setCharAt(i, målTekst.charAt(i));
+			oppgaveListe.add(new BokstavTimer(strBld.toString(), kontekst));
+		}
+		return oppgaveListe;
+	}
+
+	/**
+	 * Metode for å generere en string med "tilfeldige" bokstaver
+	 * @param strBld StringBuilder som benyttes for å bygge Stringen i
+	 * @param lengde Ønsket lengde på tilfeldig streng.
+	 */
 	private void byggRandomString(StringBuilder strBld, int lengde)
 	{
 		for(int j=0;j<=lengde;++j){
-			//if(target.charAt(j)!=' ') {
 				char random = (char) (Math.random() * (double) 'Z');
 				while ((int) random < (int) 'A' || (int) random > (int) 'z') {
 					random = (char) (Math.random() * (double) 'z');
 				}
 				strBld.append(random);
-			//} else {
-				//strBld.append(' ');
-			//}
 		}
 	}
 
+	/**
+	 * Utfør en liste med TimerTasks på en Timer. (tidsforsinkede oppgaver)
+	 * @param t liste med BokstavTimer oppgaver
+	 * @param forsinkelse Forsinkelse i millisekunder mellom hver "oppgave"
+	 */
 	private void utførTimerList(ArrayList<BokstavTimer> t, long forsinkelse)
 	{
 		Timer timer=new Timer();
@@ -63,7 +95,11 @@ public class Intro
 		}
 	}
 
-
+	/**
+	 * Timer-task.
+	 *
+	 * En oppgave som kan utføres av en timer- med en spesifisert forsinkning
+	 */
 	class BokstavTimer extends TimerTask
 	{
 		private String åSkrive;
